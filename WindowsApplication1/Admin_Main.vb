067012b04eef8729f4b044dea86586ef5f5301e8
tommodore64 <42749050+tommodore64@users.noreply.github.com>
2018-10-22 16:40:31 -0500

    Add files via upload

---
+++ WindowsApplication1/Admin_Main.vb
@@ -0,0 +1,19 @@
+﻿Public Class Admin_Main
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim form As New Admin_ViewStudentData
+        form.ShowDialog()
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim form As New Admin_ChangeExamScores
+        form.ShowDialog()
+    End Sub
+End Class
---
+++ WindowsApplication1/dbo.studentData_Chris.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[studentData_Chris] (
+    [Id]           INT          IDENTITY (1, 1) NOT NULL,
+    [student_ID]   INT          NULL,
+    [student_Name] VARCHAR (50) NULL,
+    [Exam1]        INT          NULL,
+    [Exam2]        INT          NULL,
+    [Exam3]        INT          NULL,
+	[Exam4]        INT          NULL,
+    [Class AVG]    DECIMAL (18) NULL,
+    [Course]      VARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
